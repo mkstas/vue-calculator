65d6aedf24ac004b94225fc491b83baed4413cf9
mkstas <makarov.stn@gmail.com>
2023-09-23 16:29:33 +0300

    dev: create uotput

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheHeader from "./header/TheHeader.vue";
+import TheOutput from "./output/TheOutput.vue";
 </script>
 
 <template>
   <main>
     <TheHeader />
+    <TheOutput />
   </main>
 </template>
