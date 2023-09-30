a41982479e92ccd81dc875152a79f5678a5b0f2f
mkstas <makarov.stn@gmail.com>
2023-10-01 01:39:41 +0300

    refactor: separate conditions

--- src/components/App.vue
+++ src/components/App.vue
@@ -7,28 +7,50 @@ import TheKeyboard from "./keyboard/TheKeyboard.vue";
 
 const expression: Ref<string> = ref("0");
 
-const onClickButton = (symbol: string): string => {
-  if (symbol === "AC") {
+const firstSymbolIsZero = (): boolean => {
+  return expression.value.slice(0) === "0";
+};
+
+const lengthIsOne = (): boolean => {
+  return expression.value.length === 1;
+};
+
+const clearAllExpression = (): void => {
+  expression.value = "0";
+};
+
+const calculateExpression = (): void => {
+  expression.value = eval(expression.value).toString();
+};
+
+const clearExpression = (): void | string => {
+  if ((lengthIsOne() && !firstSymbolIsZero()) || firstSymbolIsZero()) {
     return (expression.value = "0");
   }
 
-  if (symbol === "C") {
-    if (expression.value.length === 1 && expression.value.slice(0) !== "0") {
-      return (expression.value = "0");
-    }
+  expression.value = expression.value.slice(0, -1);
+};
 
-    return (expression.value = expression.value.slice(0, -1));
-  }
+const setFirstSymbol = (symbol: string): void => {
+  expression.value = symbol;
+};
 
-  if (symbol === "=") {
-    return (expression.value = eval(expression.value).toString());
-  }
+const addInExpression = (symbol: string) => {
+  expression.value += symbol;
+};
 
-  if (expression.value.slice(0) === "0") {
-    return (expression.value = symbol);
-  }
+const onClickButton = (symbol: string): string | void => {
+  if (symbol === "AC") return clearAllExpression();
 
-  return (expression.value += symbol);
+  if (symbol === "C") return clearExpression();
+
+  if (symbol === "00" && firstSymbolIsZero()) return;
+
+  if (symbol === "=") return calculateExpression();
+
+  if (firstSymbolIsZero()) return setFirstSymbol(symbol);
+
+  return addInExpression(symbol);
 };
 </script>
 
